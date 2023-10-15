26b01b446df388035baeca491f3ca296a1d4c2e3
Josh Owens <jowens@Joshs-MacBook-Air.local>
2023-10-15 16:23:38 -0500

    layout component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PubCrawler</title>
-
-        @vite(['resources/css/app.css'])
-        @livewireStyles
-    </head>
-    <body class="antialiased">
-        @livewire('hello-world')
-
-        @livewireScripts
-    </body>
-</html>
+<div id="test">
+    @livewire('hello-world')
+</div>
